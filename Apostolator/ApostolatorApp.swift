a4cc069ea32462d325b9e683c04320e84956f57f
AlexApostol <apostol516@gmail.com>
2024-05-18 08:27:07 +0200

    Initial Commit

---
+++ Apostolator/ApostolatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApostolatorApp.swift
+//  Apostolator
+//
+//  Created by Alex.personal on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ApostolatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
