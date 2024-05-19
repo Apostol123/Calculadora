302a7bbd20f52f2def43ddc7839e5be013a03d05
AlexApostol <apostol516@gmail.com>
2024-05-19 09:48:52 +0200

    add basic event tracker

--- Apostolator/ContentView.swift
+++ Apostolator/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Engine
 import DesignSystem
 
 struct ContentView: View {
@@ -13,33 +14,71 @@ struct ContentView: View {
         VStack {
             Spacer()
             HStack(spacing: 20) {
-                StandardButton(text: "AC", textColor: .black, backgroundColor: .lightGray, action: {})
-                StandardButton(text: "+/-", textColor: .black, backgroundColor: .lightGray, action: {})
-                StandardButton(text: "%", textColor: .black, backgroundColor: .lightGray, action: {})
-                StandardButton(text: "➗", textColor: .black, backgroundColor: .orange, action: {})
+                StandardButton(text: "AC", textColor: .black, backgroundColor: .customLightGray, action: {
+                    Engine.observeEvent(.action(.reset))
+                })
+                StandardButton(text: "+/-", textColor: .black, backgroundColor: .customLightGray, action: {
+                    Engine.observeEvent(.action(.toggleValueType))
+                })
+                StandardButton(text: "%", textColor: .black, backgroundColor: .customLightGray, action: {
+                    Engine.observeEvent(.action(.percentage))
+                })
+                StandardButton(text: "➗", textColor: .white, backgroundColor: .orange, action: {
+                    Engine.observeEvent(.action(.split))
+                })
             }
             HStack(spacing: 20) {
-                StandardButton(text: "7", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "8", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "9", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "X", textColor: .white, backgroundColor: .orange, action: {})
+                StandardButton(text: "7", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(7))
+                })
+                StandardButton(text: "8", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(8))
+                })
+                StandardButton(text: "9", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(9))
+                })
+                StandardButton(text: "X", textColor: .white, backgroundColor: .orange, action: {
+                    Engine.observeEvent(.action(.multiply))
+                })
             }
             HStack(spacing: 20) {
-                StandardButton(text: "4", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "5", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "6", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "-", textColor: .white, backgroundColor: .orange, action: {})
+                StandardButton(text: "4", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(4))
+                })
+                StandardButton(text: "5", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(5))
+                })
+                StandardButton(text: "6", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(6))
+                })
+                StandardButton(text: "-", textColor: .white, backgroundColor: .orange, action: {
+                    Engine.observeEvent(.action(.subtract))
+                })
             }
             HStack(spacing: 20) {
-                StandardButton(text: "1", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "2", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "3", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "+", textColor: .white, backgroundColor: .orange, action: {})
+                StandardButton(text: "1", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(1))
+                })
+                StandardButton(text: "2", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(2))
+                })
+                StandardButton(text: "3", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.value(3))
+                })
+                StandardButton(text: "+", textColor: .white, backgroundColor: .orange, action: {
+                    Engine.observeEvent(.action(.add))
+                })
             }
             HStack(spacing: 20) {
-                StandardButton(text: "0", textColor: .white, backgroundColor: .gray, width: 144, height: 60 ,action: {})
-                StandardButton(text: ".", textColor: .white, backgroundColor: .gray, action: {})
-                StandardButton(text: "=", textColor: .white, backgroundColor: .gray, action: {})
+                StandardButton(text: "0", textColor: .white, backgroundColor: .gray, width: 144, height: 60 ,action: {
+                    Engine.observeEvent(.value(0))
+                })
+                StandardButton(text: ".", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.action(.separator))
+                })
+                StandardButton(text: "=", textColor: .white, backgroundColor: .gray, action: {
+                    Engine.observeEvent(.action(.result))
+                })
             }
         }
         .padding()
@@ -52,7 +91,7 @@ struct ContentView: View {
 
 
 extension Color {
-    var lightGray: Color {
-        Color("lightGray")
+    var customLightGray: Color {
+        Color("CustomcustomLightGray")
     }
 }
--- Apostolator/Features/Engine/Sources/Engine/Engine.swift
+++ Apostolator/Features/Engine/Sources/Engine/Engine.swift
@@ -1,2 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public enum Event {
+    case value(Int)
+    case action(Action)
+}
+
+public enum Action {
+    case split
+    case multiply
+    case add
+    case subtract
+    case reset
+    case percentage
+    case toggleValueType
+    case result
+    case separator
+}
+
+
+public final class Engine {
+    public static func observeEvent(_ event: Event) {
+        print(event)
+    }
+}
+
